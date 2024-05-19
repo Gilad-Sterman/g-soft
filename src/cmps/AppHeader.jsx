93225d9402f31d25563082be4f42d12274b6400e
Gilad Sterman <giladsterman1999@gmail.com>
2024-05-19 16:58:55 +0300

    fit for mobile

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,23 +1,45 @@
-export function AppHeader({ selected, setSelected }) {
+import { useState } from "react"
+
+export function AppHeader() {
+
+    const [menuOpen, setMenuOpen] = useState(false)
 
     function scrollToTop() {
         window.scrollTo(0, 0)
     }
 
+    function toggleMenu(ev) {
+        ev.stopPropagation()
+        setMenuOpen(!menuOpen)
+    }
+    
+    function closeMenu() {
+        setMenuOpen(false)
+    }
+
     return (
-        <header className="app-header">
+        <header className="app-header" onClick={closeMenu}>
             <div className="logo" onClick={() => scrollToTop()}>
                 <img src="https://res.cloudinary.com/dollaguij/image/upload/v1716107339/logo-no-background_p7a7kq.png" alt="logo" />
             </div>
-            <nav>
+            <nav className={menuOpen ? 'open' : ''}>
                 <a href="#projects">Projects</a>
                 <a href="#technologies">Technologies</a>
                 <a href="#process">Process</a>
                 <a href="#about">About</a>
+                <button className="header-contact mobile">
+                    <span>Contact Me</span>
+                    <img src="https://res.cloudinary.com/dollaguij/image/upload/v1716108050/icons8-email-50_k3jpwc.png" alt="email" />
+                </button>
             </nav>
             <button className="header-contact">
                 <span>Contact Me</span>
                 <img src="https://res.cloudinary.com/dollaguij/image/upload/v1716108050/icons8-email-50_k3jpwc.png" alt="email" />
+            </button>
+            <button className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={(ev) => toggleMenu(ev)}>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
             </button>
         </header>
     )
--- src/cmps/Process.jsx
+++ src/cmps/Process.jsx
@@ -4,27 +4,33 @@ export function Process() {
     const steps = [
         {
             title: 'Initial Consultation',
-            description: 'Discuss the project requirements, goals, and timelines to ensure a clear understanding of the client’s needs.'
+            description: 'Discuss the project requirements, goals, and timelines to ensure a clear understanding of the client’s needs.',
+            icon: "https://res.cloudinary.com/dollaguij/image/upload/v1716126287/icons/Frame_87_jelyie.png"
         },
         {
             title: 'Project Proposal',
-            description: 'Provide a detailed proposal outlining the project scope, deliverables, timeline, and cost estimate.'
+            description: 'Provide a detailed proposal outlining the project scope, deliverables, timeline, and cost estimate.',
+            icon: "https://res.cloudinary.com/dollaguij/image/upload/v1716126283/icons/Frame_87_1_p2ua0a.png"
         },
         {
             title: 'Design & Development',
-            description: 'Create design mockups for approval, then proceed with the development phase, ensuring continuous communication and updates.'
+            description: 'Create design mockups for approval, then proceed with the development phase, ensuring continuous communication and updates.',
+            icon: "https://res.cloudinary.com/dollaguij/image/upload/v1716126283/icons/Frame_87_2_mgzamy.png"
         },
         {
             title: 'Testing & Feedback',
-            description: 'Conduct thorough testing to identify and fix any issues, and incorporate client feedback to refine the project.'
+            description: 'Conduct thorough testing to identify and fix any issues, and incorporate client feedback to refine the project.',
+            icon: "https://res.cloudinary.com/dollaguij/image/upload/v1716126285/icons/Frame_87_4_rozrqj.png"
         },
         {
             title: 'Project Delivery',
-            description: 'Deliver the completed project, provide necessary documentation, and offer training if required.'
+            description: 'Deliver the completed project, provide necessary documentation, and offer training if required.',
+            icon: "https://res.cloudinary.com/dollaguij/image/upload/v1716126286/icons/Frame_87_5_p9wliw.png"
         },
         {
             title: 'Post-Launch Support',
-            description: 'Provide ongoing support and maintenance to ensure the project continues to run smoothly.'
+            description: 'Provide ongoing support and maintenance to ensure the project continues to run smoothly.',
+            icon: "https://res.cloudinary.com/dollaguij/image/upload/v1716126284/icons/Frame_87_3_bcpapx.png"
         }
     ]
 
@@ -36,8 +42,11 @@ export function Process() {
             <div className="process-steps">
                 {steps.map((step, index) => (
                     <div key={index} className="process-step" data-aos="flip-up" data-aos-duration="1000">
-                        <h3>{step.title}</h3>
-                        <p>{step.description}</p>
+                        <img src={step.icon} alt="icon" />
+                        <div className="info">
+                            <h3>{step.title}</h3>
+                            <p>{step.description}</p>
+                        </div>
                         {index < steps.length - 1 && <div className="arrow">&#8595;</div>}
                     </div>
                 ))}
