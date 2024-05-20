afa612043ae5622f99b8363c97d151aa7e0d55c9
Gilad Sterman <giladsterman1999@gmail.com>
2024-05-20 10:58:14 +0300

    style choice

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -27,7 +27,7 @@ export function AppFooter() {
                 </nav>
                 <div className="links">
                     <a href="https://github.com/Gilad-Sterman" target="_blank" rel="noopener noreferrer">
-                        <img src="https://res.cloudinary.com/dollaguij/image/upload/v1715847543/svg/github_3291695_r9nldi.png" alt="GitHub" className="social-icon" />
+                        <img src="https://res.cloudinary.com/dollaguij/image/upload/v1716191784/icons/github_vpxvru.png" alt="GitHub" className="social-icon" />
                     </a>
                     <a href="https://www.linkedin.com/in/gilad-sterman-7b2469278/" target="_blank" rel="noopener noreferrer">
                         <img src="https://res.cloudinary.com/dollaguij/image/upload/v1715847533/svg/linkedin_3536505_hchtos.png" alt="LinkedIn" className="social-icon" />
